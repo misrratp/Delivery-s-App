27944a5b1cfcac30d8b957228a2dd1b8edd693e1
Abel <abelmihjail@gmail.com>
2022-06-14 16:42:18 -0500

    Diseño de collections page, collection detail y de place detail

--- lib/src/features/presentation/place_detail_page/View/place_detail_page.dart
+++ lib/src/features/presentation/place_detail_page/View/place_detail_page.dart
@@ -1,4 +1,6 @@
 import 'package:deliver_app_yt/src/colors/colors.dart';
+import 'package:deliver_app_yt/src/features/presentation/commons_widgets/BackButtons/back_button.dart';
+import 'package:deliver_app_yt/src/features/presentation/commons_widgets/Headers/header_double.dart';
 import 'package:deliver_app_yt/src/features/presentation/commons_widgets/Headers/headers_text.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +10,12 @@ class PlaceDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+      backgroundColor: orange,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+         label: headerText(texto: 'Añadir a la cesta 95.40', color: Colors.white, fontWeight: FontWeight.w600, fontSize: 17)),
       body: CustomScrollView(
         slivers: <Widget>[
           SliverAppBar(
@@ -29,7 +37,46 @@ class PlaceDetailPage extends StatelessWidget {
                 height: 350
               ),
               Wrap(children: [
-                Container(
+                    _promoButton(),  _infoPlace(), _infoPlaceStats(), _offerBanner()
+              ],)
+                ],
+              ),
+            ),
+            leading: Builder(builder: (BuildContext context) {
+                return backButton(context, Colors.white);
+            }),
+            actions: [
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Image(
+                  width: 28,
+                  height: 28,
+                  image: AssetImage('assets/share.png'),
+                ),
+              ),
+              IconButton(onPressed: (){}, icon: Icon(Icons.bookmark_border, color: Colors.white, size: 30,))
+            ],
+
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                _headers(texto: 'Populares'), 
+                _sliderCards(),
+                _headers(texto: 'Full Menu'),
+                _menuList(context),
+                SizedBox(height: 40.0,)
+                ]
+              ))
+          
+        ],
+      ),
+    );
+  }
+}
+
+Widget _promoButton(){
+  return Container(
                   margin: EdgeInsets.only(top: 121.0, left: 30.0, right: 15.0),
                   width: double.infinity,
                   height: 25,
@@ -45,14 +92,226 @@ class PlaceDetailPage extends StatelessWidget {
                       )
                     ],
                   ),
-                )
-              ],)
-                ],
-              ),
+                );
+}
+
+Widget _infoPlace() {
+      return Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 30.0),
+            margin: EdgeInsets.symmetric(vertical: 7.0),
+            child: headerText(texto: "Boon Lay Ho Huat Fried Prawn Noodle", color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30.0),
+          ), 
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 30.0),
+            
+            child: Row(
+              children: [
+                Icon(Icons.location_on, color: gris),
+                headerText(texto: "03 Jameson Manors Apt. 177", color: gris, fontWeight: FontWeight.w500, fontSize: 15.0),
+              ],
             ),
           )
         ],
+      );
+}
+
+Widget _infoPlaceStats(){
+  return Container(
+    margin: EdgeInsets.only(top: 26.0),
+    padding: EdgeInsets.symmetric(horizontal: 40.0),
+    height: 70.0,
+  decoration: BoxDecoration(
+    border: Border(
+    top: BorderSide(color: Colors.white),
+    bottom: BorderSide(color: Colors.white)
+    )
+  ),
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(children: [
+            Icon(
+              Icons.star,
+              color: Colors.white,
+              size: 19.0,
+            ),
+            headerText(texto: '4.5', color: Colors.white, fontWeight: FontWeight.bold, fontSize: 15.0)
+          ],),
+          headerText(texto: '351 Ratings', color: gris, fontWeight: FontWeight.w500, fontSize: 15.0)
+        ],
       ),
-    );
-  }
+      Container(
+        height: 40,
+        decoration: BoxDecoration(
+        border: Border(right: BorderSide(color: Colors.white))
+        ),
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(children: [
+            Icon(
+              Icons.bookmark,
+              color: Colors.white,
+              size: 19.0,
+            ),
+            headerText(texto: '137k', color: Colors.white, fontWeight: FontWeight.bold, fontSize: 15.0)
+          ],),
+          headerText(texto: 'Favourites', color: gris, fontWeight: FontWeight.w500, fontSize: 15.0)
+        ],
+      ),
+      Container(
+        height: 40,
+        decoration: BoxDecoration(
+        border: Border(right: BorderSide(color: Colors.white))
+        ),
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(children: [
+            Icon(
+              Icons.photo,
+              color: Colors.white,
+              size: 19.0,
+            ),
+            headerText(texto: '345', color: Colors.white, fontWeight: FontWeight.bold, fontSize: 15.0)
+          ],),
+          headerText(texto: 'Photos', color: gris, fontWeight: FontWeight.w500, fontSize: 15.0)
+        ],
+      )
+    ],
+  ),
+  );
+}
+
+Widget _offerBanner(){
+  return Container(
+    color: Color.fromRGBO(255, 237, 214, 1.0),
+    padding: EdgeInsets.all(20),
+    height: 90.0,
+    child: Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start ,
+          children: [
+            headerText(texto: 'New Try Pickup', color: orange, fontWeight: FontWeight.bold, fontSize: 15.0),
+            headerText(texto: 'Pickup on your time. Your order is \n ready when you are', color: primaryColor, fontWeight: FontWeight.w400, fontSize: 13.0)
+            
+          ],
+        ),
+        Spacer(),
+        RaisedButton(
+                      elevation: 0.5,
+                      onPressed: (){},
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                      color: orange,
+                      child: headerText(texto: 'Order now', color: Colors.white, fontWeight: FontWeight.bold, fontSize: 13.0),)
+      ],
+    ),
+  );
+}
+
+Widget _headers({texto: String}){
+  return Container(
+    margin: EdgeInsets.only(top: 20.0, bottom: 5.0),
+    padding: EdgeInsets.symmetric(horizontal: 20),
+    child: headerDoubleText(textHeader: texto, textAction: '')
+  );
+}
+
+Widget _sliderCards(){
+ return Container(
+    height: 210.0,
+    padding: EdgeInsets.only(left: 10),
+    child: ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (BuildContext context, int index){
+        return _cards();
+      }));
+}
+
+Widget _cards(){
+  return Container(
+    margin: EdgeInsets.all(8.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+       ClipRRect(
+               borderRadius: BorderRadius.circular(8.0),
+              child: Image(
+                width: 200.0,
+                height: 100.0,
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                'https://images.unsplash.com/photo-1569246294372-ed319c674f14?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+              )),
+             ),
+             Container(
+              margin: EdgeInsets.only(top: 10.0),
+              child: headerText(texto: "Penaut Chaat with Dahi",fontWeight: FontWeight.bold, fontSize: 15.0)
+              ),
+              Container(
+              alignment: Alignment.centerLeft,
+              child: headerText(texto: "9.50",fontWeight: FontWeight.w500,color: gris, fontSize: 14.0)
+              ),
+              Row(
+                children: [
+                  Container(
+              margin: EdgeInsets.only(top: 10),
+              child: headerText(texto: "Selecciona",fontWeight: FontWeight.w500,color: orange, fontSize: 15.0)
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10, left: 90),
+                child: Image(
+                  width: 20.0,
+                  height: 20.0,
+                  fit: BoxFit.cover,
+                image: AssetImage('assets/plus_order.png'),
+                ),
+              )
+                ],
+              )
+    ]),
+  );
+
+}
+
+Widget _menuList(BuildContext context){
+  return Container(
+    padding: EdgeInsets.only(left: 10.0),
+    child: Column(
+       children: [ 
+        _menuItem(context, 'Salads', '2'),
+        _menuItem(context, 'Chicken', '5'),
+        _menuItem(context, 'Soups', '6'),
+        _menuItem(context, 'Vegetables', '7')
+         
+       ],
+    ),
+  );
+}
+
+Widget _menuItem(BuildContext context, String text, String itemCount) {
+  return Container(
+    decoration: BoxDecoration(border: Border(bottom: BorderSide(color: gris ))
+    ),
+    child: Column(
+      children: [
+        ListTile(
+          title: headerText(texto: text, fontWeight: FontWeight.w300, fontSize: 17.0),
+          trailing: headerText(texto: itemCount, fontWeight: FontWeight.w300, fontSize: 17.0),
+        ),
+        _sliderCards()
+      ],
+    ),
+);
 }
